87610a52ee021ed7bf9faa128c51a872cfe31a91
Nirmean Sawi <nirmean@Nirmeans-MacBook-Pro.local>
2024-01-02 14:13:03 +0000

    DOM event listenener added to sco res js

---
+++ starter/assets/js/scores.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const highscoresList = document.getElementById("highscores");
+    const clearButton = document.getElementById("clear");
